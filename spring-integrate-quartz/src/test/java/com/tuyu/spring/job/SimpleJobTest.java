511fe7e9a67a62173bd2a177bcfe958055113883
tuyu <scutuyu@foxmail.com>
2018-05-29 17:19:56 +0800

    spring与quartz集成

---
+++ spring-integrate-quartz/src/test/java/com/tuyu/spring/job/SimpleJobTest.java
@@ -0,0 +1,55 @@
+package com.tuyu.spring.job;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * <pre>
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //             佛祖保佑       永无BUG     永不修改                   //
+ * ////////////////////////////////////////////////////////////////////
+ * </pre>
+ * <p>
+ * tuyu于5/29/18祈祷...
+ *
+ * @author tuyu
+ * @date 5/29/18
+ * Stay Hungry, Stay Foolish.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:quartzConfigure.xml"})
+public class SimpleJobTest {
+
+    /**
+     * 在配置文件中中配置quartz的调度器，触发器以及任务
+     * @throws InterruptedException
+     */
+    @Test
+    public void testXml() throws InterruptedException {
+        Thread.sleep(20000L);
+    }
+
+}
